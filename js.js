7b93936b3cfc26704231130a66b8576b44ba80c5
JulenLopez <44410338+JulenLopez@users.noreply.github.com>
2022-11-11 20:51:01 +0100

    Terminado falta de pruebas

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
-
-//Inucializacion de elementos del html
+//Inicialización de elementos del html
+//Todos los inputs utilizados y asignados
 const cajaNombre = document.getElementById("nombre");
 const cajaCorreo = document.getElementById("correo");
 const cajaPass = document.getElementById("pass");
@@ -7,8 +7,15 @@ const cajaPass2 = document.getElementById("pass2");
 const cajaDireccion = document.getElementById("direccion");
 const cajaTarjeta = document.getElementById("tarjeta");
 
+//Label para invocar el input de tarjeta
 const labelTarjeta = document.getElementById("labelTarjeta");
 
+//Todos los elementos de la "alerta" custom
+const alertCentro = document.getElementById("alertCentro");
+const cuadroAlert = document.getElementById("cuadroAlert");
+const alertTexto = document.getElementById("alertTexto");
+
+//Diferentes p para argumentar los fallos del usuario
 const falloNombre = document.getElementById("falloNombre");
 const falloCorreo = document.getElementById("falloCorreo");
 const falloPass = document.getElementById("falloPass");
@@ -16,133 +23,151 @@ const falloPass2 = document.getElementById("falloPass2");
 const falloDireccion = document.getElementById("falloDireccion");
 const falloTarjeta = document.getElementById("falloTarjeta");
 
+//Botones de html
 const botonEnviar = document.getElementById("botonEnviar");
+const botonAceptar = document.getElementById("botonAceptar");
 
-//Inicializacion de variables
-const regeNombre= /^(\w+(\s+\w+)?\s*)$/;
-const regeCorreo= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-const regePass= /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+//Inicialización de variables
+//Cración de las diferentes Regex para tenerlas a mano ante los cambios futuros
+const regeNombre = /^(\w+(\s+\w+)?\s*)$/;
+const regeCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const regePass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
 const regeVisa = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
 const regeMastercard = /^(?:5[1-5][0-9]{14})$/;
 const regeDireccion = /Calle\s*(\b\w*\b\s+){1,5}Nº\s*\d{1,5}/i;
 
-let nombreOk=false;
-let correoOk=false;
-let passOk=false;
-let pass2Ok=false;
-let direccionOk=true;
-let tarjetaOk=true;
+//Variables para la comprobación de los valores correctos, variables opcionales inicializadas a true
+let nombreOk = false;
+let correoOk = false;
+let passOk = false;
+let pass2Ok = false;
+let direccionOk = true;
+let tarjetaOk = true;
 
-//Asignacion de eventos
-cajaNombre.addEventListener("blur", (event) => {nombre();});
-cajaNombre.addEventListener("focus",function(){cajaNombre.style.color="";}, true);
+//Asignación de eventos divido a pares por elemento, obtención y perdida del foco
+cajaNombre.addEventListener("blur", (event) => { nombre(); });
+cajaNombre.addEventListener("focus", function () { cajaNombre.style.color = ""; }, true);
 
-cajaCorreo.addEventListener("blur", (event) => {correo();});
-cajaCorreo.addEventListener("focus",function(){cajaCorreo.style.color="";}, true);
+cajaCorreo.addEventListener("blur", (event) => { correo(); });
+cajaCorreo.addEventListener("focus", function () { cajaCorreo.style.color = ""; }, true);
 
-cajaPass.addEventListener("blur", (event) => {pass();});
-cajaPass.addEventListener("focus",function(){cajaPass.style.color="";}, true);
+cajaPass.addEventListener("blur", (event) => { pass(); });
+cajaPass.addEventListener("focus", function () { cajaPass.style.color = ""; }, true);
 
-cajaPass2.addEventListener("blur", (event) => {
-    if(cajaPass.value==cajaPass2.value && cajaPass){
-        cajaPass2.style.color="green";
-        falloPass2.style.display="none";
-        pass2Ok=true;
-    }else{
-        cajaPass2.style.color="red";
-        falloPass2.style.display="";
-        pass2Ok=false;
-    }});
-cajaPass2.addEventListener("focus",function(){cajaPass2.style.color="";}, true);
-
-cajaDireccion.addEventListener("blur", (event) => {direccion();});
-cajaDireccion.addEventListener("focus",function(){cajaDireccion.style.color="";}, true);
-
-cajaTarjeta.addEventListener("blur", (event) => {tarjeta();});
-cajaTarjeta.addEventListener("focus",function(){cajaTarjeta.style.color="";}, true);
-
-botonEnviar.addEventListener("click",function(){
-    if(nombreOk&&correoOk&&passOk&&pass2Ok&&!direccionOk&&!tarjetaOk){
-
-    }else{
-        
+cajaPass2.addEventListener("blur", (event) => {//No es necesario crear una función debido a la facilidad del evento
+    if (cajaPass.value == cajaPass2.value && passOk) {
+        cajaPass2.style.color = "green";
+        falloPass2.style.display = "none";
+        pass2Ok = true;
+    } else {
+        cajaPass2.style.color = "red";
+        falloPass2.style.display = "";
+        pass2Ok = false;
     }
-},false)
+});
+cajaPass2.addEventListener("focus", function () { cajaPass2.style.color = ""; }, true);
 
-//Creacion de funciones.
-function nombre(){
-    if(regeNombre.test(cajaNombre.value)){
-        cajaNombre.style.color="green";
-        falloNombre.style.display="none";
-        nombreOk=true;
+cajaDireccion.addEventListener("blur", (event) => { direccion(); });
+cajaDireccion.addEventListener("focus", function () { cajaDireccion.style.color = ""; }, true);
+
+cajaTarjeta.addEventListener("blur", (event) => { tarjeta(); });
+cajaTarjeta.addEventListener("focus", function () { cajaTarjeta.style.color = ""; }, true);
+
+alertCentro.addEventListener("click", function () { alertCentro.style.display = "none"; }, false);
+cuadroAlert.addEventListener("click", function () { alertCentro.style.display = "none"; }, false);
+
+//Eventos de los botones, están bastante planos debido a la falta de implementación de elementos
+botonEnviar.addEventListener("click", function () {
+    if (nombreOk && correoOk && passOk && pass2Ok && direccionOk && tarjetaOk) {
+        alertTexto.innerHTML = "Usuario creado";
+        alertCentro.style.display = "";
+        //TODO Crear un objeto con la información del formulario para poder enviarlo contra la BBDD cuando se implemente
+    } else {
+        alertTexto.innerHTML = "Todos los campos obligatorios deben estar rellenados y correctos";
+        alertCentro.style.display = "";
+    }
+}, false);
+botonAceptar.addEventListener("click", function () {
+    //TODO Redirigir al home page
+}, false);
+
+//Creación de funciones. 
+//TODO Interesante crear una función para todos debido a la alta repetición de código 
+//Dar una vuelta para comprobar asequibilidad 
+//Eliminación de más del 50% de líneas de las funciones 
+function nombre() {
+    if (regeNombre.test(cajaNombre.value)) {
+        cajaNombre.style.color = "green";
+        falloNombre.style.display = "none";
+        nombreOk = true;
         return;
     } else {
-        cajaNombre.style.color="red";
-        falloNombre.style.display="";
-        nombreOk=false;
+        cajaNombre.style.color = "red";
+        falloNombre.style.display = "";
+        nombreOk = false;
         return;
     }
 }
 
-function correo(){
-    if(regeCorreo.test(cajaCorreo.value)){
-        cajaCorreo.style.color="green";
-        falloCorreo.style.display="none";
-        correoOk=true;
+function correo() {
+    if (regeCorreo.test(cajaCorreo.value)) {
+        cajaCorreo.style.color = "green";
+        falloCorreo.style.display = "none";
+        correoOk = true;
         return;
     } else {
-        cajaCorreo.style.color="red";
-        falloCorreo.style.display="";
-        correoOk=false;
+        cajaCorreo.style.color = "red";
+        falloCorreo.style.display = "";
+        correoOk = false;
         return;
     }
 }
 
-function pass(){
-    if(regePass.test(cajaPass.value)){
-        cajaPass.style.color="green";
-        falloPass.style.display="none";
-        passOk=true;
+function pass() {
+    if (regePass.test(cajaPass.value)) {
+        cajaPass.style.color = "green";
+        falloPass.style.display = "none";
+        passOk = true;
         return;
     } else {
-        cajaPass.style.color="red";
-        falloPass.style.display="";
-        passOk=false;
+        cajaPass.style.color = "red";
+        falloPass.style.display = "";
+        passOk = false;
         return;
     }
 }
 
-function direccion(){
-    if(regeDireccion.test(cajaDireccion.value)){
-        cajaDireccion.style.color="green";
-        falloDireccion.style.display="none";
-        labelTarjeta.style.display="";
-        passOk=true;
+function direccion() {//Función distinta al resto debido al display, tener en cuenta
+    if (regeDireccion.test(cajaDireccion.value)) {
+        cajaDireccion.style.color = "green";
+        falloDireccion.style.display = "none";
+        labelTarjeta.style.display = "";//Poner después/antes de la función global
+        passOk = true;
         return;
     } else {
-        cajaDireccion.style.color="red";
-        falloDireccion.style.display="";
-        labelTarjeta.style.display="none";
-        passOk=false;
+        cajaDireccion.style.color = "red";
+        falloDireccion.style.display = "";
+        labelTarjeta.style.display = "none";//Poner después/antes de la función global
+        passOk = false;
         return;
     }
 }
 
-function tarjeta(){
-    if(regeVisa.test(cajaTarjeta.value.trim())){
-        cajaTarjeta.style.color="green";
-        falloTarjeta.style.display="";
-        tarjetaOk=true;
+function tarjeta() {
+    if (regeVisa.test(cajaTarjeta.value.trim())) {
+        cajaTarjeta.style.color = "green";
+        falloTarjeta.style.display = "";
+        tarjetaOk = true;
         return;
-    }else if(regeMastercard.test(cajaTarjeta.value.trim())){
-        cajaTarjeta.style.color="green";
-        falloTarjeta.style.display="";
-        tarjetaOk=true;
+    } else if (regeMastercard.test(cajaTarjeta.value.trim())) {
+        cajaTarjeta.style.color = "green";
+        falloTarjeta.style.display = "";
+        tarjetaOk = true;
         return;
-    }else{
-        cajaTarjeta.style.color="red";
-        falloTarjeta.style.display="none";
-        tarjetaOk=false;
+    } else {
+        cajaTarjeta.style.color = "red";
+        falloTarjeta.style.display = "none";
+        tarjetaOk = false;
         return;
     }
 }
